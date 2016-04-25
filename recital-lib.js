6914d52a04d19e9a49976c07cc13acd8ea6a95d6
RECITAL <huangxing1105@gmail.com>
2016-04-25 14:36:50 +0800

    component img loader

--- recital-lib.js
+++ recital-lib.js
@@ -327,3 +327,165 @@ function isDate(input) {
 
     $.fn.inputWithCounter.defaults = defaults;
 })(window, window.$);
+
+// component: img loader
+!(function (window, $, undefined) {
+    if (!($ && $.fn)) {
+        return;
+    }
+
+    var lastestSrc;
+    var timer;
+
+    function clearTimer() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    }
+
+    var defaults = {
+        SERVER_URL: '',
+        afterAdaptive: undefined
+    };
+
+    var ImgLoader = function (container, options) {
+        var self = this;
+
+        self.$ = $(container);
+
+        self.options = $.extend(true, {}, defaults, options);
+
+        self.init();
+
+        return self;
+    };
+
+    ImgLoader.prototype = {
+        init: function () {
+            var self = this;
+
+            self.lastestSrc = null;
+
+            self.$.addClass('img-container');
+
+            if (typeof self.$.attr('avatar') !== 'undefined') {
+                self.$.addClass('img-avatar');
+                self.avatar = true;
+            }
+
+            if (typeof self.$.attr('adaptive') !== 'undefined') {
+                self.$.addClass('img-adaptive');
+                self.adaptive = true;
+            }
+
+            if (self.$.attr('img-src')) {
+                self.src = $.trim(self.$.attr('img-src'));
+                self.load();
+            }
+
+            if ($.fn.imgZoomable) {
+                self.$.on('click', 'img,.img', function () {
+                    $(this).imgZoomable();
+                });
+            }
+        },
+
+        isSrcChanged: function () {
+            return this.lastestSrc !== this.src;
+        },
+
+        load: function () {
+            var self = this;
+            var src = self.src;
+
+            clearTimer();
+
+            if (!src) {
+                // 5秒后显示图片加载失败
+                timer = setTimeout(function () {
+                    self.$.removeClass('img-loading img-loaded').addClass('img-error');
+                }, 5000);
+                return;
+            }
+
+            if (typeof src === 'string' && src[0] === '/') {
+                src = self.options.SERVER_URL + src;
+            }
+
+            self.$.html('').removeClass('img-loaded img-error').addClass('img-loading');
+
+            var $img = $('<div />').addClass('img').css('opacity', 0).appendTo(self.$);
+
+            var img = document.createElement('img');
+            img.src = src;
+            img.onload = onLoad;
+            img.onerror = onError;
+
+            function clear() {
+                $img = null;
+                img = null;
+            }
+
+            function onError() {
+                if (self.isSrcChanged()) {
+                    self.$.removeClass('img-loading');
+                    self.$.addClass('img-error');
+                    $img.remove();
+                }
+
+                clear();
+            }
+
+            function onLoad() {
+                if (self.isSrcChanged()) {
+                    self.$.removeClass('img-loading');
+                    self.$.addClass('img-loaded');
+
+                    if (self.adaptive) {
+                        var oldSize, newSize;
+
+                        oldSize = {
+                            w: self.$.width(),
+                            h: self.$.height()
+                        };
+
+                        self.$.append(img);
+                        $img.remove();
+
+                        newSize = {
+                            w: self.$.width(),
+                            h: self.$.height()
+                        };
+
+                        if ($.isFunction(self.options.afterAdaptive)) {
+                            self.options.afterAdaptive(newSize, oldSize);
+                        }
+                    }
+                    else {
+                        $img.css({'background-image': 'url(' + src + ')', opacity: 1});
+                    }
+                    self.lastestSrc = self.src;
+                }
+
+                clear();
+            }
+        }
+    };
+
+    $.fn.imgLoader = function (options) {
+        var firstInstance;
+
+        this.each(function () {
+            var inst = new ImgLoader(this, options);
+
+            if (!firstInstance) {
+                firstInstance = inst;
+            }
+        });
+
+        return firstInstance;
+    };
+
+    $.fn.imgLoader.defaults = defaults;
+}(window, window.$));
